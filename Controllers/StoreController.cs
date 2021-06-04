9adad295a7424f14c6d52de26e48ac9ca1c11366
Elomelo 320 <szymongooogle@gmail.com>
2021-06-04 21:52:35 +0200

    Skończona część 3

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -4,46 +4,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using MyMusicStoreTutorial.Models;
 
 namespace MyMusicStoreTutorial.Controllers
 {
     public class StoreController : Controller
     {
+        IEnumerable<Genre> genres;
+        public StoreController()
+        {
+            genres = new List<Genre>
+            {
+                new Genre { Name = "Disco"},
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+        }
         /// <summary>
         /// GET: /Store/
         /// </summary>
         /// <returns></returns>
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello from Store.Index()";
+            return View(genres);
         }
-        ///// <summary>
-        ///// GET: /Store/Browse
-        ///// </summary>
-        ///// <returns></returns>
-        //public string Browse()
-        //{
-        //    return "Hello from Store.Browse()";
-        //}
 
         /// <summary>
         /// /Store/Browse?genre=Disco
         /// </summary>
         /// <param name="genre">Argument GET-a</param>
         /// <returns></returns>
-        public string Browse(string genre)
+        public ActionResult Browse(string genre)
         {
-            return HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return View(new Genre { Name = genre });
         }
 
 
-        /// <summary>
-        /// GET: /Store/Details/id
-        /// </summary>
-        /// <returns></returns>
-        public string Details(int id)
+    /// <summary>
+    /// GET: /Store/Details/id
+    /// </summary>
+    /// <returns></returns>
+    public ActionResult Details(int id)
         {
-            return "Store.Details, ID = " + id;
+             return View(new Album { Title = "Album " + id });
         }
     }
 }
